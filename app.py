72df5b38d3e1b3aea71516a9b48b5f118480d6a3
Muddssir44 <102971524+Muddssir44@users.noreply.github.com>
2024-10-29 01:20:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome to my Flask application! Enjoy exploring."
+    return "Welcome to my Flask application!!! Enjoy exploring."
 
 @app.route('/how-are-you')
 def hello():
